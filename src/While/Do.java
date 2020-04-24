66f821dc361c9e843ff47b1c7769c63e1d0a0e91
Rofiah123 <62419412+Rofiah123@users.noreply.github.com>
2020-04-25 00:18:42 +0700

    salah

--- src/While/Do.java
+++ src/While/Do.java
@@ -1,5 +1,19 @@
 package While;
 
 public class Do {
-    isiAir();
-}while(isiGelas != penuh);
+    int isiGelas = 0;
+    int penuh = 225;
+
+    System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+    System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+    do {
+        isiGelas++;
+        System.out.println("Sedang mengisi gelas..");
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+    }while(isiGelas != penuh);
+
+System.out.println("Finale: Gelas Sekarang : " + isiGelas + "ml");
+
+ }
+}
