11a86d7ec275b1b5895a48f4f4781c321471e108
Rofiah123 <62419412+Rofiah123@users.noreply.github.com>
2020-04-26 22:47:41 +0700

    teman

---
+++ src/Tugas/Array.java
@@ -0,0 +1,12 @@
+package Tugas;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] teman = {"fiah", "aznur", "riska", "dinah"};
+
+        for (int i = 0; i < teman.length; i++) {
+            System.out.println("Indeks ke-" + i + ": " + teman[i]);
+        }
+    }
+}
